0f1247724ace8b7f4dc7fd8a5d7daecd2c912c92
Thiagodoy <thiagodoy@hotmail.com>
2020-01-02 22:23:32 -0300

    Criação do endpoint Repository para subir os processDefinition

--- MatrixCore/src/main/java/com/core/matrix/utils/Utils.java
+++ MatrixCore/src/main/java/com/core/matrix/utils/Utils.java
@@ -5,27 +5,43 @@
  */
 package com.core.matrix.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
  * @author thiag
  */
-
 @Component
 public class Utils {
-    
+
     private static BCryptPasswordEncoder bcpe;
-    
-    
-    static{
+
+    static {
         bcpe = new BCryptPasswordEncoder();
+    }
+
+    public static String encodePassword(String in) {
+        return bcpe.encode(in);
     }
     
     
-  public static  String encodePassword(String in)  {
-      return bcpe.encode(in);
-  }
-    
+    public static synchronized InputStream generateInpuStream(MultipartFile file) throws FileNotFoundException, IOException{
+        
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();       
+        
+        return new FileInputStream(convFile);
+        
+    }
+
 }
