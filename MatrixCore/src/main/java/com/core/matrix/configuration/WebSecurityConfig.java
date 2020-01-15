1dadf5abac86099eb04140021a242c2a9cbbd50d
Thiagodoy <thiagodoy@hotmail.com>
2020-01-14 22:15:41 -0300

    Liberação de url

--- MatrixCore/src/main/java/com/core/matrix/configuration/WebSecurityConfig.java
+++ MatrixCore/src/main/java/com/core/matrix/configuration/WebSecurityConfig.java
@@ -68,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth")
+                .antMatchers("/api/auth","/api/workflow/repository/diagram")
                 .permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "**")
                 .permitAll()
