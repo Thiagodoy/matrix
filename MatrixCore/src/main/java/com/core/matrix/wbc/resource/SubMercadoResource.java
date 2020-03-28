f16c9631762a56c02c9b034b63e5453142e4b1d0
Thiagodoy <thiagodoy@hotmail.com>
2020-03-28 16:19:56 -0300

    Ajustes

--- MatrixCore/src/main/java/com/core/matrix/wbc/resource/SubMercadoResource.java
+++ MatrixCore/src/main/java/com/core/matrix/wbc/resource/SubMercadoResource.java
@@ -5,15 +5,11 @@
  */
 package com.core.matrix.wbc.resource;
 
-import com.core.matrix.response.PageResponse;
 import com.core.matrix.wbc.dto.SubMercadoDTO;
 import com.core.matrix.wbc.service.SubMercadoService;
 import java.util.List;
 import java.util.logging.Level;
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
