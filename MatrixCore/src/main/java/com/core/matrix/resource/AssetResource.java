45997f240ffb2dee8a8cc45e259150e1b7776daf
Thiagodoy <thiagodoy@hotmail.com>
2020-05-04 16:14:08 -0300

    Ajuste na versão

--- MatrixCore/src/main/java/com/core/matrix/resource/AssetResource.java
+++ MatrixCore/src/main/java/com/core/matrix/resource/AssetResource.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/asset")
 public class AssetResource {
 
-    private String version = "v1.1.9";
+    private String version = "v1.2.0";
 
     @RequestMapping(value = "/versao", method = RequestMethod.GET)
     public ResponseEntity getVersao() throws IOException {
